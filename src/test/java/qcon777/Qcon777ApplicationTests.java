8f504acad73d542acd5c0aa8a097eb43d3a1200d
Matt Cadorette <mcadorette@gmail.com>
2018-11-06 10:34:47 -0800

    Initial commit from Atomist

---
+++ src/test/java/qcon777/Qcon777ApplicationTests.java
@@ -0,0 +1,16 @@
+package qcon777;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Qcon777ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
